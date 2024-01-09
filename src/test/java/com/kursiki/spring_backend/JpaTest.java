be96ab69e5534c6d4d6705c9d404ff03d553b5b3
Vaskin_D <vaskin_d@utkonos.ru>
2024-01-09 20:14:45 +0300

    final

--- src/test/java/com/kursiki/spring_backend/JpaTest.java
+++ src/test/java/com/kursiki/spring_backend/JpaTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = Application.class)
-@DataJpaTest
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class JpaTest {
 
 	@Test
